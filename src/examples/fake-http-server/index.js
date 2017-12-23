89c8ebd6c559f996061b9b7aa7bed6845b3ba314
restuta <restuta8@gmail.com>
2017-12-22 17:05:37 -0800

    add fake http server for testing

---
+++ src/examples/fake-http-server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const PORT = 3456
+
+const { rnd } = require('../../lib/utils/rnd')
+
+const sendFail = (res, number) =>
+  res.status(500).send(`💥 You got broken ${number}!`)
+
+const sendSometimesFailingResponse = (res, number) => {
+  const randomNumber = rnd(1, 3)
+
+  return randomNumber === 3 ? sendFail(res, number) : res.send(number)
+}
+
+app.get('/numbers/:number', (req, res) => {
+  const paramsNumber = req.params.number
+  // return sendSometimesFailingResponse(res, paramsNumber)
+  return paramsNumber === '3' ? sendFail(res, paramsNumber) : res.send(paramsNumber)
+})
+
+const startHttpServer = () =>
+  app.listen(PORT, () => console.log(`Example app listening on port PORT!`))
+
+module.exports = {
+  startHttpServer,
+}
